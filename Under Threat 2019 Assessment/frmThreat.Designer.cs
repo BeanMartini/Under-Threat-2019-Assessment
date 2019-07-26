cc53a0f661204ab521ec450c0cfd9a4f90253e98
Ben Martin <benmartin@mmc.school.nz>
2019-07-26 13:05:12 +1200

    Added text boxes and labels for title, score, lives etc

--- Under Threat 2019 Assessment/frmThreat.Designer.cs
+++ Under Threat 2019 Assessment/frmThreat.Designer.cs
@@ -32,6 +32,13 @@
             this.pnlGame = new System.Windows.Forms.Panel();
             this.tmrMissile = new System.Windows.Forms.Timer(this.components);
             this.tmrPerson = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLives = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // pnlGame
@@ -54,11 +61,86 @@
             this.tmrPerson.Interval = 50;
             this.tmrPerson.Tick += new System.EventHandler(this.tmrPerson_Tick);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(109, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(396, 73);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Under Threat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(679, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(667, 162);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 43);
+            this.txtName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(682, 286);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 28);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Score";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(702, 314);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(26, 28);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(685, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 28);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lives";
+            // 
+            // txtLives
+            // 
+            this.txtLives.AutoSize = true;
+            this.txtLives.Font = new System.Drawing.Font("Bauhaus 93", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLives.Location = new System.Drawing.Point(702, 454);
+            this.txtLives.Name = "txtLives";
+            this.txtLives.Size = new System.Drawing.Size(26, 28);
+            this.txtLives.TabIndex = 2;
+            this.txtLives.Text = "5";
+            // 
             // frmThreat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(834, 549);
+            this.Controls.Add(this.txtLives);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pnlGame);
             this.DoubleBuffered = true;
             this.KeyPreview = true;
@@ -68,6 +150,7 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmThreat_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmThreat_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,6 +159,13 @@
         private System.Windows.Forms.Panel pnlGame;
         private System.Windows.Forms.Timer tmrMissile;
         private System.Windows.Forms.Timer tmrPerson;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label txtLives;
     }
 }
 
